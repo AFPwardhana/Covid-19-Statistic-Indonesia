11a532d499bad763c01f77f3a4f29a9f87f034a5
semua data <sampah.semuadata@gmail.com>
2020-09-07 19:40:12 +0700

    another try to deploy

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
+const port =  process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
-
-const server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
+app.use(express.static(__dirname));
+app.get('*',(req,res) =>{
+  res.sendFile(path.resolve(__dirname,'index.html'))
 });
+
+
+app.listen(port);
+console.log("started");
